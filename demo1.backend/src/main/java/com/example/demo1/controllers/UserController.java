bb4c51f40f7ca224085603753661d94fe31823f8
pbisevac <61277977+pbisevac@users.noreply.github.com>
2023-05-07 14:31:11 +0200

    Revert "Task 2"

--- demo1.backend/src/main/java/com/example/demo1/controllers/UserController.java
+++ demo1.backend/src/main/java/com/example/demo1/controllers/UserController.java
@@ -1,9 +1,6 @@
 package com.example.demo1.controllers;
 
-import com.example.demo1.models.UserModel;
-import com.example.demo1.services.IUserService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,26 +10,13 @@ import java.util.List;
 @RequestMapping("user")
 public class UserController {
 
-    private IUserService iUserService;
-
-    public UserController(IUserService iUserService){
-        this.iUserService=iUserService;
-    }
     @GetMapping("home")
     public String Home(){
         return "Home Page";
     }
 
     @GetMapping("get")
-    public List<UserModel> GetAll(){
-        return iUserService.GetAll();
-    }
-    @PostMapping("create")
-    public UserModel Create(String firstName, String lastName,String email){
-        UserModel model =new UserModel();
-        model.setFistName(firstName);
-        model.setLastName(lastName);
-        model.setEmail(email);
-        return iUserService.Create(model);
+    public List<String> GetAll(){
+        return List.of("Sasa", "Marko", "Ivko");
     }
 }
--- demo1.backend/src/main/java/com/example/demo1/repositories/IUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo1.repositories;
-
-import com.example.demo1.models.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserRepository extends JpaRepository<UserModel, Integer> {
-}
